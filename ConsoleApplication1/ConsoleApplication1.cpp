b3d03d441784efe46e4b95e829b1fc605519d48f
therealpersong <timotheussoares5@gmail.com>
2024-02-19 23:58:38 +0000

    FirstCommitForVS_1

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
-int Multiply()
+int Multiply(int a, int b)
 {
-	return 5 * 8
+	return a * b;
 }
 
 int main()
 {
 	std::cout << "Hello world" << std::endl;
+	//commit testing
 	std::cin.get();
 }
