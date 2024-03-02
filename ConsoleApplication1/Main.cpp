56ea6d1577bff8795f944350c149d39153af5418
therealpersong <timotheussoares5@gmail.com>
2024-03-02 19:25:35 +0000

    Nots,Ors,Ands,XORS,NANDs,

--- ConsoleApplication1/Main.cpp
+++ ConsoleApplication1/Main.cpp
@@ -3,11 +3,14 @@
 int main()
 {
     int i = 0;
-
+    bool condition = true;
     for ( ; i < 5;)
     {
         std::cout << "Hello World" << std::endl;
         i++;
+        if (!(i < 5))
+            condition = false;
     }
+    std::cout << condition << std::endl;
     return 0;
 }
